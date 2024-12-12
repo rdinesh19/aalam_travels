8ccef16e506d36695fbba306f617106c64f51262
rdinesh19 <rdineshpp@gmail.com>
2024-12-12 13:21:23 +0530

    debug banner hide

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       child: MaterialApp(
-      
+        debugShowCheckedModeBanner: false,
            home: LoginPage(),
       ),
     );
